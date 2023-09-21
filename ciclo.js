3dc69255439c0d9ea8f767ab3858591f3388111a
leoncomolli <140461809+leoncomolli@users.noreply.github.com>
2023-09-20 23:23:29 -0300

    Add files via upload

---
+++ ciclo.js
@@ -0,0 +1,15 @@
+/* contador de numero x a numero x (ascendente) ciclo for */
+
+for (cont=0; cont<=10; cont++) {
+    console.log("Contando numero: " + cont);
+}
+console.log("Fin del dictado")
+
+/* contador pero con ciclo while je*/
+
+var contador = 0;
+while (contador<10) {
+    console.log ("Estoy en la repeticion numero " + contador);
+    contador++;
+}
+console.log("Fin de las vueltas")
---
+++ funciones.js
@@ -0,0 +1,8 @@
+/* no se me ocurrio algun ejemplo practico asi que
+dejo algunas operaciones hechas con funciones */
+
+function calcularPromedio(num1, num2, num3){
+    let promedio = (num1 + num2 + num3)/3;
+    return "El promedio es: " + promedio + " pesos";
+}
+console.log(calcularPromedio(35, 10, 9));
